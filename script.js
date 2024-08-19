d62dc59320c91f258de67d38a8b0e3721ad31abf
eugen <eugenbeirit22@gmail.com>
2024-08-19 07:41:59 +0200

    update

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-Schritt-1 Zuerst sieht der Benutzer einen Terminal großen Terminal vor sich und er soll sich eine Zahl 
-zwischen 2 und eine 10 aussuchen dabei er darf sie niemandem mitteilen.
-
-Schritt-2 Benutzer soll seine Zahl die er sich gemerkt hat, mit seiner Zahl addieren.
-
-Schritt-3 Benutzer soll die Zahl mit 2 multiplizieren.
-
-Schritt-4 Benutzer soll die Zahl durch seine Zahl teilen.
-
-Schritt-5 Benutzer soll schon die Antwort sehen und die ist seine Endantwort.
-
-Schritt-6 Benutzer soll die Seite neu laden können und nochmal von vorne anfangen.
-
-Schritt-7 Benutzer soll die Anweisungen sehen die er erledigen soll.
-*/
-
 let randomNumber = Math.floor(Math.random() * 9) + 2;
 let result = randomNumber * 2;
 
